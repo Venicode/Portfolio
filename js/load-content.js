84fad1d4aef9e619cbc6ce9f1e646e218236e76e
venicode <vitoria_aparecida2011@hotmail.com>
2024-06-28 17:47:15 -0300

    load projects first

--- js/load-content.js
+++ js/load-content.js
@@ -1,8 +1,23 @@
 const buttonProject = document.getElementById('project');
 const buttonAboutMe = document.getElementById('aboutme');
 const contentDiv = document.getElementById('content');
-
+buttonProject.click();
 document.addEventListener('DOMContentLoaded', ()=>{
+    fetch('../src/projects.html')
+    .then(response =>{
+        if(!response.ok){
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.text();
+    })
+    .then(data =>{
+        contentDiv.innerHTML = data;
+    })
+    .catch(error =>{
+        console.error('There has been a problem with your fetch operation:', error);
+        contentDiv.innerHTML = '<p>Sorry, there was an error loading the content.</p>';
+    })
+    
     buttonProject.addEventListener('click', ()=>{
         fetch('../src/projects.html')
         .then(response =>{
